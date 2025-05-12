87bff0c8cfc02a4a6ff0979e93b746335cedba33
crystalchoi <sujeungchoi@gmail.com>
2025-05-12 21:18:52 +0900

    init

---
+++ chap02/hello.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+let count = 0;
+
+const server = http.createServer((req, res) => {
+	log(count);
+	res.status = 200;
+	// res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader("Content-Type", "text/plain");
+	res.write("Hello world!");
+	res.write(JSON.stringify(count));
+	setTimeout(() => {
+		res.end('Hello world!');
+		}, 2000);
+})
+
+
+function log(count) {
+	console.log((count += 1)); // ❷
+}
+
+server.listen(8000, () => console.log('listening on port 8080'));
